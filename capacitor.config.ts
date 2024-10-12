36885eeea9d4391fe207e2138188d6b7049237cf
ben wainaina <bwainainamwangi@gmail.com>
2024-10-12 11:33:35 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: '21.ionic-fix-keyboard-pushing-content-up',
+  webDir: 'www'
+};
+
+export default config;
